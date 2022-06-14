7eef3ad28dbeed95e75a98f233f336b80b22ec00
felipeganame <2111762@ucc.edu.ar>
2022-06-14 18:26:58 -0300

    Se termino ejercicio de grados_radianes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,24 @@
  * @param string} valor - El id de los inputs de metros-yardas-pies-pulgadas
  * @return
  */
+
+
+
+
+
+
+
+
+
+function convertirGr(id){
+  var grad, rad;
+  fi(id=="grados"){
+    grad = document.getElementById(elementid:"grados").value;
+    rad = (grad*Math.PI)/180;
+  }else if(id==radianes){
+    rad = document.getElementById(elementid:"grados").value;
+    grad = (rad*180)/Math.PI;
+  }
+  document.getElementById(elementid: "grados").value = grad;
+  document.getElementById(elementid: "grados").value = rad;
+}
