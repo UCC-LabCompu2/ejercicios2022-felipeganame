4462ba0b6a6ba4ee2e4cddb9a15a5f494fe0c0bb
felipeganame <2111762@ucc.edu.ar>
2022-06-14 22:28:03 -0300

    se realizo el ejercicio de mostrar_oculatar

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -2,7 +2,7 @@
  * Conversion de unidades, de metros-yardas-pies-pulgadas
  * @method cambiar unidades
  * @param {string} id - El id de los inputs de metros-yardas-pies-pulgadas
- * @param string} valor - El id de los inputs de metros-yardas-pies-pulgadas
+ * @param {string} valor - El id de los inputs de metros-yardas-pies-pulgadas
  * @return
  */
 
@@ -11,18 +11,23 @@
 
 
 
-
-
-
-function convertirGr(id){
+function convertirGR(id){
   var grad, rad;
-  fi(id=="grados"){
-    grad = document.getElementById(elementid:"grados").value;
+  if(id=="grados"){
+    grad = document.getElementById("grados").value;
     rad = (grad*Math.PI)/180;
-  }else if(id==radianes){
-    rad = document.getElementById(elementid:"grados").value;
+  }else if(id=="radianes"){
+    rad = document.getElementById("grados").value;
     grad = (rad*180)/Math.PI;
   }
-  document.getElementById(elementid: "grados").value = grad;
-  document.getElementById(elementid: "grados").value = rad;
+  document.getElementById("grados").value = grad;
+  document.getElementById("grados").value = rad;
+}
+
+function mostrar_ocultar(valorMO){
+  if(valorMO=="val_mostrar"){
+    document.getElementById("divMO").style.display = 'block';
+  }else if(valorMO=="val_ocultar"){
+    document.getElementById("divMO").style.display = 'none';
+  }
 }
